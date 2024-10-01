3d8d3e11aa76ac4e4bbe305f2901d41d08ec2a9f
Mohamed Abd Elhakime <you@example.com>
2024-10-02 01:45:31 +0300

    create ReviewsRepoImpl to service Locator.

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -4,6 +4,7 @@ import 'package:get_it/get_it.dart';
 import '../../features/childern/data/repositories/childern_repo_impl.dart';
 import '../../features/home/data/repositories/home_repo_impl.dart';
 import '../../features/login/data/repositories/login_repo_impl.dart';
+import '../../features/reviews/data/repositories/reviews_repo_impl.dart';
 import '../../features/schoolCode/data/repositories/school_code_repo_impl.dart';
 import 'api_service.dart';
 
@@ -31,6 +32,11 @@ void setupServiceLocator() {
   );
   getIt.registerSingleton<ChildernRepoImpl>(
     ChildernRepoImpl(
+      getIt.get<ApiService>(),
+    ),
+  );
+  getIt.registerSingleton<ReviewsRepoImpl>(
+    ReviewsRepoImpl(
       getIt.get<ApiService>(),
     ),
   );
