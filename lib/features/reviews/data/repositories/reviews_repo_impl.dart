e48b5cf77715296de6a8dadac0563c0b0fabb81d
Mohamed Abd Elhakime <you@example.com>
2024-10-02 01:43:45 +0300

    create reviews_repo_impl.dart

---
+++ lib/features/reviews/data/repositories/reviews_repo_impl.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/helper/SharedPreferences/pref.dart';
+import '../../../../core/utils/api_service.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../models/reviews_model.dart';
+import 'reviews_repo.dart';
+
+class ReviewsRepoImpl implements ReviewsRepo {
+  ApiService apiService;
+
+  ReviewsRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, ReviewsModel>> getReviews() async {
+    try {
+      String schoolDomain =
+          await Pref.getStringFromPref(key: AppStrings.schoolDomainKey) ?? "";
+      String token =
+          await Pref.getStringFromPref(key: AppStrings.parantTokenKey) ?? "";
+      int childernId =
+          await Pref.getIntFromPref(key: AppStrings.childernIdKey) ?? -1;
+      Map<String, dynamic> data = await apiService.get(
+        host: schoolDomain,
+        endPoint: "student/getReviews?student_id=$childernId",
+        headers: {
+          "Authorization": "Bearer $token",
+        },
+      );
+      ReviewsModel reviewsModel = ReviewsModel.fromJson(data);
+      return right(reviewsModel);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+}
