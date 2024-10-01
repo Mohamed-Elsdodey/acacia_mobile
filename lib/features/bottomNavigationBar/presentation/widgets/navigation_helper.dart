3108e032b79275a1684ae067a87dc33d81513fbc
Mohamed Abd Elhakime <you@example.com>
2024-10-02 02:57:55 +0300

    add reviews view to navigation helper and finish it.

--- lib/features/bottomNavigationBar/presentation/widgets/navigation_helper.dart
+++ lib/features/bottomNavigationBar/presentation/widgets/navigation_helper.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../childern/presentation/views/childern_view.dart';
 import '../../../more/presentation/views/more_view.dart';
+import '../../../reviews/presentation/views/reviews_view.dart';
 
 class NavigationHelper {
   static Widget getMyWidget(int index) {
@@ -10,11 +11,7 @@ class NavigationHelper {
       case 1:
         return const HomeView();
       case 2:
-        return const Scaffold(
-          body: Center(
-            child: Text("2"),
-          ),
-        );
+        return const ReviewsView();
       case 3:
         return const ChildernView();
       case 4:
